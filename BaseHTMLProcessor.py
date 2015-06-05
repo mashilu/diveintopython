a5a9c49160deee86fdcbd08797d8c77c384aa725
mashilu <mslbuaa@163.com>
2015-06-06 00:14:58 +0800

    BaseHTMLProcessor

--- BaseHTMLProcessor.py
+++ BaseHTMLProcessor.py
@@ -17,3 +17,17 @@ class BaseHTMLProcessor(SGMLParser):
         
     def handle_entityref(self, ref):
         self.pieces.append("&%(ref)s" % locals())
+        if htmlentitydefs.entitydefs.has_key(ref):
+            self.pieces.append(";")
+            
+    def handle_data(self, text):
+        self.pieces.append(text)
+        
+    def handle_comment(self, text):
+        self.pieces.append("<!--%(test)s-->" % locals())
+        
+    def handle_pi(self, text):
+        self.pieces.append("<?%(text)s>" % locals())
+        
+    def handle_decl(self, text):
+        self.pieces.append("<!%(text)s>" % locals())
